2c7943b6c711c8e5a83fab0e6933946f60c480e2
SimonIT <simonit.orig@gmail.com>
2019-04-30 00:11:40 +0200

    adding german locale

---
+++ lib/src/locale/german.dart
@@ -0,0 +1,68 @@
+part of duration.locale;
+
+class GermanDurationLocale implements DurationLocale {
+  const GermanDurationLocale();
+
+  String year(int amount, [bool abbreviated = true]) {
+    if (abbreviated)
+      return 'J';
+    else
+      return 'Jahr' + (amount > 1 ? 'e' : '');
+  }
+
+  String month(int amount, [bool abbreviated = true]) {
+    if (abbreviated)
+      return 'Mon';
+    else
+      return 'Monat' + (amount > 1 ? 'e' : '');
+  }
+
+  String week(int amount, [bool abbreviated = true]) {
+    if (abbreviated)
+      return 'W';
+    else
+      return 'Woche' + (amount > 1 ? 'n' : '');
+  }
+
+  String day(int amount, [bool abbreviated = true]) {
+    if (abbreviated)
+      return 'T';
+    else
+      return 'Tag' + (amount > 1 ? 'e' : '');
+  }
+
+  String hour(int amount, [bool abbreviated = true]) {
+    if (abbreviated)
+      return 'S';
+    else
+      return 'Stunde' + (amount > 1 ? 'n' : '');
+  }
+
+  String minute(int amount, [bool abbreviated = true]) {
+    if (abbreviated)
+      return 'M';
+    else
+      return 'Minute' + (amount > 1 ? 'n' : '');
+  }
+
+  String second(int amount, [bool abbreviated = true]) {
+    if (abbreviated)
+      return 'S';
+    else
+      return 'Sekunde' + (amount > 1 ? 'n' : '');
+  }
+
+  String millisecond(int amount, [bool abbreviated = true]) {
+    if (abbreviated)
+      return 'Ms';
+    else
+      return 'Millisekunde' + (amount > 1 ? 'n' : '');
+  }
+
+  String microseconds(int amount, [bool abbreviated = true]) {
+    if (abbreviated)
+      return 'Us';
+    else
+      return 'Mikrosekunde' + (amount > 1 ? 'n' : '');
+  }
+}

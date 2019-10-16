7d36183b6372c92dcc0066356fb760190f99ed36
onatcipli <onatcipli@outlook.com>
2019-10-16 13:53:31 +0300

    remove unnecessary const keywords

--- lib/src/time_const.dart
+++ lib/src/time_const.dart
@@ -1,23 +1,23 @@
 /// Duration of a microsecond
-const Duration aMicrosecond = const Duration(microseconds: 1);
+const Duration aMicrosecond = Duration(microseconds: 1);
 
 /// Duration of a millisecond
-const Duration aMillisecond = const Duration(milliseconds: 1);
+const Duration aMillisecond = Duration(milliseconds: 1);
 
 /// Duration of a second
-const Duration aSecond = const Duration(seconds: 1);
+const Duration aSecond = Duration(seconds: 1);
 
 /// Duration of a minute
-const Duration aMinute = const Duration(minutes: 1);
+const Duration aMinute = Duration(minutes: 1);
 
 /// Duration of an hour
-const Duration anHour = const Duration(hours: 1);
+const Duration anHour = Duration(hours: 1);
 
 /// Duration of a day
-const Duration aDay = const Duration(days: 1);
+const Duration aDay = Duration(days: 1);
 
 /// Duration of a week
-const Duration aWeek = const Duration(days: 7);
+const Duration aWeek = Duration(days: 7);
 
 // Creates a [Duration] with given [milliseconds]
 Duration us(int microseconds) => Duration(microseconds: microseconds);
